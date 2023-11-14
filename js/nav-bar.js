651e36554dd36b40ebc7a9a6c7c8f0301ee9f753
Akpa Solomon <solomonakpas@gmail.com>
2023-11-14 23:15:16 +0100

    fourth commit

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,8 +1,16 @@
 const activePage = window.location.pathname;
 const navLinkEls = document.querySelectorAll('.nav_link');
 
+// navLinkEls.forEach(navLinkEls => {
+//   if (navLinkEls.href.includes(activePage)) {
+//     navLinkEls.classList.add('active');
+//   }
+// });
+
 navLinkEls.forEach(navLinkEls => {
-  if (navLinkEls.href.includes(activePage)) {
+  const navlinkPathname = new URL(navLinkEls.href).pathname;
+
+  if ((activePage === navlinkPathname) || (activePage === '/index.html' && navlinkPathname === '/')) {
     navLinkEls.classList.add('active');
   }
-});
+})
